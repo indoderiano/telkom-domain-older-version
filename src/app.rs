466606a8c7ae42817dc18bbcfc756fe8df486c53
hackerrank <me@hackerrank.com>
2021-07-10 02:38:30 +0700

    add store

--- src/app.rs
+++ src/app.rs
@@ -2,13 +2,15 @@ use yew::prelude::*;
 use yew_router::prelude::*;
 use yew_router::components::RouterAnchor;
 // use yew::services::ConsoleService;
-// use yewdux::prelude::WithDispatch;
+use yewdux::prelude::WithDispatch;
 // use yewdux::prelude::*;
 // use yewtil::NeqAssign;
 
 use crate::pages::{
     home::Home,
     details::Details,
+    reducer_global::ReducerGlobal,
+
 };
 
 
@@ -41,36 +43,27 @@ impl Component for App {
     }
 
     fn view(&self) -> Html {
-        // let render = Router::render(|switch: Route| match switch {
-        //     Route::Home => html! {<Home/>},
-        //     Route::Details => html! {<Details/>},
-        // });
+        let render = Router::render(|switch: Route| match switch {
+            Route::Home => html! {<Home/>},
+            Route::Details => html! {<Details/>},
+        });
         type Anchor = RouterAnchor<Route>;
         html! {
             <div>
                 <p>{ "Hello world!" }</p>
-                // <Anchor route=Route::Home classes="item">
-                //   {"Home"}
-                // </Anchor>
-                // <Anchor route=Route::Details classes="item">
-                //   {"Details"}
-                // </Anchor>
-                // <main>
-                //     <Router<Route, ()> render=render/>
-                // </main>
+                <Anchor route=Route::Home classes="item">
+                  {"Home"}
+                </Anchor>
+                <Anchor route=Route::Details classes="item">
+                  {"Details"}
+                </Anchor>
+                <main>
+                    <Router<Route, ()> render=render/>
+                </main>
+                <p></p>
+                <p>{"Reducer"}</p>
+                <WithDispatch<ReducerGlobal>/>
             </div>
         }
     }
 }
-
-
-
-// pub struct DataState {
-//     name: String,
-//     age: u8
-// }
-
-// let account = DataState {
-//     name: String::from("Indo"),
-//     age: 33
-// };
--- src/pages/mod.rs
+++ src/pages/mod.rs
@@ -1,2 +1,3 @@
 pub mod details;
 pub mod home;
+pub mod reducer_global;
