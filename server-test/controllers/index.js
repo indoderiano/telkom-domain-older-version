5e9ad42dde49061d66770f402b9677ab572aef1b
indo deriano t halim <mde50526@gmail.com>
2021-11-09 14:22:08 +0800

    wiring backend, get, create, update, and error handling, and loading elements

--- server-test/controllers/index.js
+++ server-test/controllers/index.js
@@ -1,7 +1,9 @@
 const { ControllerApi } = require('./ControllerApi')
 const { ControllerSettings } = require('./ControllerSettings')
+const { ControllerRoles } = require('./ControllerRoles')
 
 module.exports={
     ControllerApi,
     ControllerSettings,
+    ControllerRoles,
 }
