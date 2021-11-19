b3e30cfcd14092821d7095df212335187af008e2
indo deriano t halim <mde50526@gmail.com>
2021-11-20 05:13:44 +0800

    delete permission, dynamic modal delete permission, autoclose modal delete permission

--- src/pages/management/users/setting_user_permissions.rs
+++ src/pages/management/users/setting_user_permissions.rs
@@ -16,9 +16,11 @@ use yew_router::service::RouteService;
 pub struct UserTabPermissions {
     link: ComponentLink<Self>,
     fetch_task: Option<FetchTask>,
-    loading_get_user_permission: bool,
     user_permissions: Vec<UserPermissions>,
+    loading_get_user_permission: bool,
     error_user_permission_list: Option<String>,
+    show_modal_delete_permission: bool,
+    index_permission_delete: Option<usize>,
     loading_delete_permissions: bool,
     error_delete_permissions: Option<String>,
     route_service: RouteService,
@@ -33,6 +35,7 @@ pub enum Msg {
     DefaultState,
     RequestUserPermissions,
     GetUserPermissions(Vec<UserPermissions>),
+    ShowModalDeletePermission(bool, Option<usize>),
     ResponseError(String, StateError),
     Delete,
     RedirectToPermissions,  
@@ -46,9 +49,11 @@ impl Component for UserTabPermissions {
         UserTabPermissions {
             link,
             fetch_task: None,
-            loading_get_user_permission: false,
             user_permissions: Vec::new(),
+            loading_get_user_permission: false,
             error_user_permission_list: None,
+            show_modal_delete_permission: false,
+            index_permission_delete: None,
             loading_delete_permissions: false,
             error_delete_permissions: None,
             route_service: RouteService::new(),
@@ -70,6 +75,13 @@ impl Component for UserTabPermissions {
             }
             Msg::RequestUserPermissions => {
                 ConsoleService::info("ini di request user permissions");
+
+                // default state
+                self.loading_delete_permissions = false;
+                self.fetch_task = None;
+                self.show_modal_delete_permission = false;
+                self.index_permission_delete = None;
+
                 let request = Request::get(format!("{}/users/tenantid/users/:id/permissions", API_URL))
                     .header("access_token", "telkomidtelkomdomain")
                     .body(Nothing)
@@ -100,6 +112,11 @@ impl Component for UserTabPermissions {
                 self.fetch_task = None;
                 true
             }
+            Msg::ShowModalDeletePermission(state, index_selected) => {
+                self.show_modal_delete_permission = state;
+                self.index_permission_delete = index_selected;
+                true
+            }
             Msg::ResponseError(message, state) => {
                 ConsoleService::info("ini di info response error");
                 match state{
@@ -126,7 +143,7 @@ impl Component for UserTabPermissions {
                     match data{
                         Ok(dataok) => {
                             ConsoleService::info(&format!("{:?}", dataok));
-                            Msg::RedirectToPermissions
+                            Msg::RequestUserPermissions
                         }
                         Err(error) => {
                             ConsoleService::info(&error.to_string());
@@ -270,39 +287,7 @@ impl Component for UserTabPermissions {
                     }
 
 
-                    <div class="modal fade" id="permissionDeleteModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">{"Remove from Role?"}</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    {"Are you sure that you want to unassign permission 'create:client_grants'?"}
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">{"Cancel"}</button>
-                                    <button 
-                                        type="button" 
-                                        class="btn btn-danger"
-                                        onclick=self.link.callback(|_|Msg::Delete)
-                                        >{"Yes, remove"}</button>
-                                        {
-                                            if self.error_delete_permissions.is_some() {
-                                                html! {
-                                                    <div class="alert alert-warning" role="alert">
-                                                        <i class="bi bi-exclamation-triangle me-2"></i>
-                                                        { self.error_delete_permissions.clone().unwrap() }
-                                                    </div>
-                                                }
-                                            } else {
-                                                html! {}
-                                            }
-                                        }
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    { self.modal_delete_permission() }
 
                      
                     <div class="modal fade" id="addPermissions" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -342,23 +327,115 @@ impl Component for UserTabPermissions {
 
 impl UserTabPermissions {
     fn view_user_permissions(&self) -> Vec<Html> {
-        self.user_permissions.iter().map(|user|{
+        // https://stackoverflow.com/questions/58737024/how-to-get-the-index-of-the-current-element-being-processed-in-the-iteration-wit
+        self.user_permissions.iter().enumerate().map(|(i, user)|{
            html! {
                <tr>
-                       <th scope="row">{&user.permission_name}</th>
-                       <td>{&user.description}</td>
-                       <td>{&user.resource_server_name}</td>
-                       <td>{"Direct"}</td>
-                       <td>
-                           <button type="button" class="btn btn-outline-secondary px-2 py-1" data-bs-toggle="modal" data-bs-target="#permissionDeleteModal">
-                               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                                   <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                               </svg>
-                           </button>
-                       </td>
+                    <th scope="row">{&user.permission_name}</th>
+                    <td>{&user.description}</td>
+                    <td>{&user.resource_server_name}</td>
+                    <td>{"Direct"}</td>
+                    <td>
+                        <button
+                            type="button"
+                            class="btn btn-outline-secondary px-2 py-1"
+                            // data-bs-toggle="modal"
+                            // data-bs-target="#permissionDeleteModal"
+                            onclick=self.link.callback(move |_| Msg::ShowModalDeletePermission(true, Some(i)))
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                            </svg>
+                        </button>
+                    </td>
                </tr>
            }
         }).collect()
+    }
+
+    fn modal_delete_permission(&self) -> Html {
+        html! {
+            <>
+                <div
+                    class=format!("modal fade {}", if self.show_modal_delete_permission {"show"} else {""})
+                    // id="permissionDeleteModal"
+                    // tabindex="-1"
+                    // aria-labelledby="exampleModalLabel"
+                    // aria-hidden="true"
+                >
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">{"Remove from Role?"}</h5>
+                                <button
+                                    type="button"
+                                    class="btn-close"
+                                    data-bs-dismiss="modal"
+                                    aria-label="Close"
+                                    onclick=self.link.callback(move |_| Msg::ShowModalDeletePermission(false, None))
+                                ></button>
+                            </div>
+                            <div class="modal-body">
+                                {"Are you sure that you want to unassign permission '"}
+                                { 
+                                    if self.index_permission_delete.is_some() {
+                                        html! {
+                                            <>
+                                                { self.user_permissions[self.index_permission_delete.unwrap()].permission_name.clone() }
+                                            </>
+                                        }
+                                    } else {
+                                        html! {}
+                                    }
+                                }
+                                {"'?"}
+                            </div>
+                            <div class="modal-footer">
+                                <button
+                                    type="button"
+                                    class="btn btn-outline-secondary"
+                                    data-bs-dismiss="modal"
+                                    onclick=self.link.callback(move |_| Msg::ShowModalDeletePermission(false, None))
+                                >
+                                    {"Cancel"}
+                                </button>
+                                <button
+                                    type="button"
+                                    class=format!("btn {} btn-danger position-relative", if self.loading_delete_permissions {"loading"} else {""} )
+                                    onclick=self.link.callback(|_| Msg::Delete)
+                                    disabled={ self.loading_delete_permissions }
+                                >
+                                    <div class="telkom-label">
+                                        {"Yes, remove"}
+                                    </div>
+                                    <div class="telkom-spinner telkom-center">
+                                        <div class="spinner-border spinner-border-sm" role="status"/>
+                                    </div>
+                                </button>
+                            </div>
+                            {
+                                if self.error_delete_permissions.is_some() {
+                                    html! {
+                                        <div class="modal-footer">
+                                            <div class="alert alert-warning" role="alert">
+                                                <i class="bi bi-exclamation-triangle me-2"></i>
+                                                { self.error_delete_permissions.clone().unwrap() }
+                                            </div>
+                                        </div>
+                                    }
+                                } else {
+                                    html! {}
+                                }
+                            }
+                        </div>
+                    </div>
+                </div>
+                <div
+                    class=format!("modal-backdrop fade {}", if self.show_modal_delete_permission {"show"} else {""})
+                    onclick=self.link.callback(move |_| Msg::ShowModalDeletePermission(false, None))
+                />
+            </>
+        }
     }
 }
